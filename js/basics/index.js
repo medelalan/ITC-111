ce54a991919dcf40b0c3a064aea72570e33a9ffd
Alan Medel <medelalan23@gmail.com>
2020-05-07 20:15:39 -0700

    Javascript example in class 5/7/2020

---
+++ js/basics/index.js
@@ -0,0 +1,20 @@
+// console.log('Cherry Oh!');
+
+var firstName = 'John';
+
+var message = 'ITC-111'; // variable of type string
+
+var year = 2020; //variable of type number
+
+var isTrue = true; //variable of type boolean
+
+var someVar;
+
+    function printToConsole(msg){
+        //console.log(msg);
+        var what = msg + 'Javascript';
+        return what;
+    }
+
+console.log(message);
+console.log(printToConsole(message));
