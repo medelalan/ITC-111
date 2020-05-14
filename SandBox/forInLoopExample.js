eb2550842d1bf5ce9ad7e7684a506179242b9611
Alan Medel <medelalan23@gmail.com>
2020-05-14 13:58:12 -0700

    JavaScript ForIn Loop, WhileIn, Operations

---
+++ SandBox/forInLoopExample.js
@@ -0,0 +1,17 @@
+// ForIn Loop - used to loop/iterate through properties of an object
+
+// Syntax: for(property in object) {do stuff;}
+
+var user = {
+    fName:"John",
+    LName:"Johnson",
+    age:21
+};
+
+var details = '';
+var p;
+
+for (p in user){
+    details += user[p] + " ";
+}
+console.log(details);
