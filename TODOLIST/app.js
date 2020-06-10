24cb5073c39c7ecf39d7079ca29156b64faa0fc3
Alan Medel <medelalan23@gmail.com>
2020-06-09 17:45:57 -0700

    s and p changed values

---
+++ TODOLIST/app.js
@@ -0,0 +1,5 @@
+// Selecting Elements in Variables
+const clear = document.querySelector(".clear"); //selects clear button
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
